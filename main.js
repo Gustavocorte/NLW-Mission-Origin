74cbd01d09db3b649c3d01e25f93dae055158dcd
GustavoCortes <gustavocortes981@gmail.com>
2022-05-09 22:17:27 -0300

    Atualizações ate a aula 05

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+window.addEventListener('scroll', onScroll)
+
+
 function onScroll() {
-  if (scrollY > 0) {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll(){
+  if (scrollY => 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
   }
 }
 
- 
+function showBackToTopButtonOnScroll(){
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -14,3 +29,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
