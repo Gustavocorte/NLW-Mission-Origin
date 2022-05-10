642713db603250c9f274473ae5493db057f4a7f8
GustavoCortes <gustavocortes981@gmail.com>
2022-05-10 20:38:40 -0300

    final do projeto DoctorCare

--- main.js
+++ main.js
@@ -4,6 +4,58 @@ window.addEventListener('scroll', onScroll)
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section){
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // Verificar se a seção passou da linha
+  // quais dads vou precisar?
+
+  // o topo da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção seção ou ultrapassou a linha alto
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // informações dos dados e da lógica
+  console.log(
+    'O topo da seção chegou ou passou da linha?',
+     sectionTopReachOrPassedTargetline
+  )
+
+  // verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a seção termina aonde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  console.log('O fundo da seção passou da linha?', sectionEndPassedTargetline)
+
+
+// Limites da seção
+const sectionBoundaries =
+sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+const sectionId = section.getAttribute('id')
+const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+menuElement.classList.remove('active')
+if (sectionBoundaries) {
+menuElement.classList.add('active')
+}
 }
 
 function showNavOnScroll(){
